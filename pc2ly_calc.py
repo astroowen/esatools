3e03c3a4e599fa81b02737bd3f4e7bbc1db5f495
Owen Higgins <47695566+astroowen@users.noreply.github.com>
2025-10-07 17:08:03 +0200

    Make it easier to paste distances from Simbad into parsec to lightyear converter

--- pc2ly_calc.py
+++ pc2ly_calc.py
@@ -1,12 +1,11 @@
 from astropy import units as u
 
 dis = [
-19.4,
-14.3,
-14.20,
-19.8,
-11.57,
+" |   22.80    Mpc |                       |redshift|2010ApJ...725.2270P|",
+" |   21       Mpc |                       |        |2007ApJS..173..185G|",
+" |   22.39    Mpc |                       |redshift|2007ApJ...655..790C|",
     ]
+if type(dis[0]) == str: dis = [float(d.strip()[1:9]) for d in dis]
 
 inunit = u.megaparsec
 outunit = u.megalightyear
