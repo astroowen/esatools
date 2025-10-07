95644a5348a7864df9df8592204433dc56c325d3
Owen Higgins <47695566+astroowen@users.noreply.github.com>
2025-10-07 17:07:29 +0200

    Update name of angular separation calculator

--- ang_diam_calc.py
+++ ang_diam_calc.py
@@ -1,10 +1,10 @@
 import astropy.units as u
 
 # Input distance (must be in pc) from Earth to object:
-D = (160_000*u.lightyear).to(u.parsec)
+D = (2_800_000_000*u.lightyear).to(u.parsec)
 # Input angular diameter of target on image (or just of frame)
 # (note: arcmin*60 = arcsec, degree*60*60 = arcsec)
-delta = (2.65*60*u.arcsecond)
+delta = (2.78*60*u.arcsecond)
 # This calculates the physical diameter of the target (or image):
 d = (D*delta)/(206265 * u.arcsecond)
 d = d.to(u.lightyear)
