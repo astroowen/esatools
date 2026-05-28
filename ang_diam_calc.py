f81e3c5e5973f4ba88afc182372ddbda3d33cf20
Owen Higgins <47695566+astroowen@users.noreply.github.com>
2026-05-28 18:53:12 +0200

    print extra angular diameter info

--- ang_diam_calc.py
+++ ang_diam_calc.py
@@ -2,11 +2,15 @@ import astropy.units as u
 
 # Input distance (must be in pc) from Earth to object:
 #D = (2_800_000_000*u.lightyear).to(u.parsec)
-D = 1366*u.parsec
+D = 392*u.parsec
 # Input angular diameter of target on image (or just of frame)
 # (note: arcmin*60 = arcsec, degree*60*60 = arcsec)
-delta = (16*u.arcsecond)
+delta = (6.681*u.arcminute)*60
 # This calculates the physical diameter of the target (or image):
 d = (D*delta)/(206265 * u.arcsecond)
-d = d.to(u.lightyear)
-print("distance      D: ", D, "\n", "apparent size δ: ", delta, "\n", "physical size d: ", d, sep="")
+
+print(
+    "distance      D: ", D, "\n",
+    "apparent size δ: ", delta, "\n",
+    "physical size d: ", d.to(u.lightyear), "\n",
+    "                (", d.to(u.parsec), ")", sep="")
--- pc2ly_calc.py
+++ pc2ly_calc.py
@@ -1,12 +1,26 @@
 from astropy import units as u
 
-dis = [25]
+dis = [
+ "|   19.5     Mpc |  -3.9        +3.9     |T-F     |2022MNRAS.511.6160K|",
+ "|   14.32    Mpc |  -1.16       +1.16    |T-F     |2020ApJ...902..145K|",
+ "|   16.7     Mpc |  -1.2        +1.2     |kin     |2018ApJ...861...49H|",
+ "|   11.1686  Mpc |                       |redshift|2019A&A...631A..38L|",
+ "|   19.95    Mpc |                       |        |2016AJ....152...50T|",
+ "|   20.9     Mpc |                       |T-F     |2014MNRAS.444..527S|",
+ "|   19.68    Mpc |  -0.20       +0.20    |        |2013AJ....146...86T|",
+ "|   15.3     Mpc |                       |redshift|2011MNRAS.413..813C|",
+ "|   32.70    Mpc |                       |redshift|2010ApJ...725.2270P|",
+ "|   18       Mpc |  -3          +3       |T-F     |2008ApJ...676..184T|",
+ "|   20.48    Mpc |                       |redshift|2007ApJ...655..790C|",
+ "|   19.41    Mpc |                       |redshift|2007ApJ...655..790C|",
+ "|   16.7     Mpc |                       |        |2007AJ....133.2569G|",
+]
 if type(dis[0]) == str:
 	dis = [float(d.strip()[1:9]) for d in dis]
 	print("reformatted:", dis)
 
-inunit = u.parsec
-outunit = u.lightyear
+inunit = u.megaparsec
+outunit = u.megalightyear
 
 for d in dis:
     l = (d * inunit).to(outunit)
