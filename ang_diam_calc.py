30913c321546601b972f730f464510404b94608f
Owen Higgins <47695566+astroowen@users.noreply.github.com>
2026-07-17 10:06:28 +0200

    Correct conversion to arcseconds

--- ang_diam_calc.py
+++ ang_diam_calc.py
@@ -2,10 +2,10 @@ import astropy.units as u
 
 # Input distance (must be in pc) from Earth to object:
 #D = (2_800_000_000*u.lightyear).to(u.parsec)
-D = 392*u.parsec
+D = 2300*u.parsec
 # Input angular diameter of target on image (or just of frame)
-# (note: arcmin*60 = arcsec, degree*60*60 = arcsec)
-delta = (6.681*u.arcminute)*60
+# must be in arcseconds - don't multiply, use .to()
+delta = (2*u.degree).to(u.arcsecond)
 # This calculates the physical diameter of the target (or image):
 d = (D*delta)/(206265 * u.arcsecond)
 
